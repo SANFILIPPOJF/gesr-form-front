1b792b1b89d890b175665dbf63d7ff0fe040667e
SANFILIPPOJF <jean-francois.sanfilippo@sncf.fr>
2023-04-19 09:48:33 +0200

    feat: add context

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Titre } from './components/titre';
 import { Connexion } from './components/connexion';
+import { AuthContext } from './context/auth-context';
 
 export interface User {
   cp: string;
@@ -10,16 +11,19 @@ export interface User {
 }
 
 const App =  () => {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState<boolean>(false);
   const [page, setPage] = useState<String>("connexion")
   const [user, setUser] = useState<User>({
     cp: "",
     password: ""
 });
   return (
+    <AuthContext.Provider value={{ token: "", isUserLoggedIn: isUserLoggedIn }}>
     <div className="App">
       <Titre></Titre>
       {page === "connexion" && <Connexion setUser={setUser} user={user}></Connexion>}
     </div>
+    </AuthContext.Provider>
   );
 }
 export default App;
