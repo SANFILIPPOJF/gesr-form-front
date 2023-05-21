171032c09c0fa40a11d774c95537820d654aa8d9
SANFILIPPOJF <jean-francois.sanfilippo@sncf.fr>
2023-05-21 15:58:12 +0200

    feat: page admin residence

--- src/components/admin/residences.tsx
+++ src/components/admin/residences.tsx
@@ -1,29 +1,185 @@
-import { Accordion } from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
+import { Accordion, Table } from "react-bootstrap";
+import { AuthContext } from "../../context/auth-context";
+import { TResponse } from "../../types/response_type";
+import { addResidence, getResidences, inactiveResidence, updateResidence } from "../../services/Residences.service";
+import { DEFAULT_RESIDENCE } from "../../constants/default_residence";
 
 export function Residences() {
+    const { residence, residences, reload, connected,
+        setReload, setResidences, setResidence }
+        = useContext(AuthContext);
+    const [residenceName, setResidenceName] = useState<string>("");
+    useEffect(() => {
+
+        const residencesData = async () => {
+            const response: TResponse = await getResidences(connected.token);
+            if (response.statusCode < 300) {
+                setResidences([...response.data]);
+            }
+        }
+        residencesData();
+    }, [reload])
+
+    const residencesTab = residences.map((res, index) => {
+        return (
+            <tr key={index}>
+                <td>{res.name}</td>
+            </tr>
+        )
+    })
+
+    const residencesSelect = residences.map((residence, index) => {
+        return (
+            <option key={index} value={index}>{residence.name}</option>
+        )
+    })
+
+    const submitNewResidence = async (name: string) => {
+        if (name === "") {
+            return alert("No residence to submit")
+        }
+        const response = await addResidence(name)
+        if (response.statusCode === 201) {
+            setResidences([...residences, response.data])
+            setResidenceName("");
+            setReload(!reload);
+        } else alert(response.message)
+    }
+
+    const submitUpdateResidence = async () => {
+        if (residence.id === 0) {
+            return alert("Select a residence")
+        } else {
+            if (residenceName === "") {
+                return alert("Nothing to update")
+            }
+            residence.name = residenceName;
+            const response = await updateResidence(residence);
+            if (response.statusCode === 200) {
+                const newResidences = residences.map(r => {
+                    if (r.id === residence.id) r = residence;
+                    return r
+                })
+                setResidences(newResidences);
+                resetValues();
+                setReload(!reload);
+            } else alert(response.message)
+        }
+    }
+
+    const deleteResidence = async () => {
+        if (residence.id === 0) {
+            return alert("Select a residence")
+        }
+        const response = await inactiveResidence(residence.id);
+        if (response.statusCode === 200) {
+            const newResidences = residences.filter((r) => r.id !== residence.id)
+            setResidences(newResidences)
+            resetValues();
+            setReload(!reload);
+        } else alert(response.message)
+    }
+
+    const resetValues = () => {
+        setResidence(DEFAULT_RESIDENCE);
+        setResidenceName("");
+    }
+
+    const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+        const propertyValue = e.currentTarget.value;
+        setResidenceName(propertyValue);
+    };
     return (
         <Accordion>
             <Accordion.Item eventKey="0">
-                <Accordion.Header>Liste des residences</Accordion.Header>
+                <Accordion.Header>Liste des Residences</Accordion.Header>
                 <Accordion.Body>
+                    <div className="overflow-auto">
+                        <Table striped>
+                            <thead>
+                                <tr>
+                                    <th>Nom</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {residencesTab}
+                            </tbody>
+                        </Table>
+                    </div>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
-                <Accordion.Header>Ajouter une residence</Accordion.Header>
+                <Accordion.Header>Ajouter une Residence</Accordion.Header>
                 <Accordion.Body>
-
+                    <form className="container w-100"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            submitNewResidence(residenceName);
+                            e.currentTarget.reset()
+                        }}>
+                        <div className="form-floating mb-3">
+                            <input
+                                onChange={handleChange}
+                                name="name"
+                                type="name"
+                                className="form-control"
+                                id="nameInput"
+                            />
+                            <label htmlFor="floatingInput">Nom de la residence</label>
+                        </div>
+                        <button className="btn btn-primary" type="submit">Soumettre
+                        </button>
+                    </form>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="2">
-                <Accordion.Header>Modifier une residence</Accordion.Header>
+                <Accordion.Header>Modifier une Residence</Accordion.Header>
                 <Accordion.Body>
-
+                    <form className="container w-100"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            submitUpdateResidence();
+                            e.currentTarget.reset()
+                        }}>
+                        <div>
+                            <select className="form-select form-select-sm mb-3"
+                                onChange={(e) => setResidence(residences[+e.target.value])} size={5} aria-label=".form-select-lg example">
+                                {residencesSelect}
+                            </select>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input
+                                onChange={handleChange}
+                                name="name"
+                                type="name"
+                                className="form-control"
+                                id="nameInput"
+                            />
+                            <label htmlFor="floatingInput">Nom de la residence</label>
+                        </div>
+                        <button className="btn btn-primary" type="submit">Soumettre
+                        </button>
+                    </form>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="3">
-                <Accordion.Header>Supprimer une residence</Accordion.Header>
+                <Accordion.Header>Supprimer une Residence</Accordion.Header>
                 <Accordion.Body>
-
+                <form onSubmit={(e) => {
+                        e.preventDefault();
+                        deleteResidence();
+                        e.currentTarget.reset()
+                    }}>
+                        <div>
+                            <select className="form-select form-select-sm mb-3"
+                                onChange={(e) => setResidence(residences[+e.target.value])} size={5} aria-label=".form-select-lg example">
+                                {residencesSelect}
+                            </select>
+                        </div>
+                        <button className="btn btn-primary" type="submit">Supprimer
+                        </button>
+                    </form>
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion >
--- src/services/Residences.service.ts
+++ src/services/Residences.service.ts
@@ -1,9 +1,39 @@
 import { BASEURL } from "../constants/base_url";
+import { Residence } from "../types/residence_type";
 import { TResponse } from "../types/response_type";
 
 
 export const getResidences = async (token: string): Promise<TResponse> => {
     const options = { method: 'GET', headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` } };
     const response = await fetch(`${BASEURL}/residences/`, options);
+    return await response.json();
+}
+
+export const addResidence = async (name: string): Promise<TResponse> => {
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: `{"name":"${name}"}`
+    };
+    const response = await fetch(`${BASEURL}/residences/`, options)
+    return await response.json();
+}
+
+export const updateResidence = async (residence: Residence): Promise<TResponse> => {
+    const options = {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: `{"name":"${residence.name}"}`
+    };
+    const response = await fetch(`${BASEURL}/residences/${residence.id}`, options)
+    return await response.json();
+}
+
+export const inactiveResidence = async (residenceId: number): Promise<TResponse> => {
+    const options = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+    };
+    const response = await fetch(`${BASEURL}/residences/${residenceId}`, options)
     return await response.json();
 }
